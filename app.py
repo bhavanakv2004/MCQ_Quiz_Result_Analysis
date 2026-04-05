13a703444a1486ab582b11bc5f740e9b9d96e60d
bhavanakv2004 <bhavanakv2004@gmail.com>
2026-04-05 20:49:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.set_page_config(page_title="Quiz Analytics", layout="wide")
+
+st.title("📊 Smart Quiz Analytics Dashboard")
