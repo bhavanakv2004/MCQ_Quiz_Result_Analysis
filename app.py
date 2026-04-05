78e4b4a13128f6812d70adbe5eac5ad93d105f83
bhavanakv2004 <bhavanakv2004@gmail.com>
2026-04-05 21:13:47 +0530

    Implement file upload for CSV and Excel files
    Added file uploader to upload CSV or Excel files and read them into a DataFrame.

--- app.py
+++ app.py
@@ -6,4 +6,18 @@ import seaborn as sns
 st.set_page_config(page_title="Quiz Analytics", layout="wide")
 
 st.title("📊 Smart Quiz Analytics Dashboard")
+# --Upload-- #
+file = st.file_uploader("Upload CSV or Excel File", type=["csv", "xlsx"])
 
+if file:
+    # Read file
+    if file.name.endswith(".csv"):
+        df = pd.read_csv(file)
+    else:
+        df = pd.read_excel(file)
+
+    df.columns = df.columns.str.strip()
+
+    st.success("File uploaded successfully")
+
+   
