fb29630da6cb029b8a9a7057383ec4aaa6c5749c
bhavanakv2004 <bhavanakv2004@gmail.com>
2026-04-05 21:32:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,22 +29,22 @@ def login():
             st.error("Invalid Username or Password ❌")
 #------------------ MAIN APP -------------------------#
 def main_app():
-st.set_page_config(page_title="Quiz Analytics", layout="wide")
-
-st.title("📊 Smart Quiz Analytics Dashboard")
-# --Upload-- #
-file = st.file_uploader("Upload CSV or Excel File", type=["csv", "xlsx"])
-
-if file:
-    # Read file
-    if file.name.endswith(".csv"):
-        df = pd.read_csv(file)
-    else:
-        df = pd.read_excel(file)
-
-    df.columns = df.columns.str.strip()
-
-    st.success("File uploaded successfully")
+    st.set_page_config(page_title="Quiz Analytics", layout="wide")
+    
+    st.title("📊 Smart Quiz Analytics Dashboard")
+    # --Upload-- #
+    file = st.file_uploader("Upload CSV or Excel File", type=["csv", "xlsx"])
+    
+    if file:
+        # Read file
+        if file.name.endswith(".csv"):
+            df = pd.read_csv(file)
+        else:
+            df = pd.read_excel(file)
+    
+        df.columns = df.columns.str.strip()
+    
+        st.success("File uploaded successfully")
 
    # ---Detect questions ---- #
     question_cols = [col for col in df.columns if col.startswith("Q")]
